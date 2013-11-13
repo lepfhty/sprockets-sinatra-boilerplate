0f6bed434c206f84017f4637e1be89f87c0b7149
Ricky Nguyen <ricky.nguyen@gmail.com>
2013-11-13 00:14:35 -0800

    add some comments

--- app.rb
+++ app.rb
@@ -6,17 +6,21 @@ require 'coffee-script'
 require 'eco'
 
 class App < Sinatra::Base
+  # in production, compress assets
   set :assets_css_compressor, :yui
   set :assets_js_compressor, :uglifier
 
+  # in development, serve individual files
   configure :test, :development do
     Sprockets::Helpers.configure do |config|
       config.debug = true
     end
   end
 
+  # integrate Sprockets
   register Sinatra::AssetPipeline
 
+  # your app here
   get '/' do
     slim :index
   end
